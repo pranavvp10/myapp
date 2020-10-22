e24a6c7fdcb8d6b83b5809002fc74abbb873d72f
pranav vp <pranavvp07@gmail.com>
2020-10-22 17:50:48 +0530

    Updates

---
+++ precache-manifest.3e88119bcaee596a7c8bbb319eb9c2ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a8a5f8a69989a909350",
+    "url": "/myapp/static/js/main.9b6c3609.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "6d45bfe45f433fb0eb2e",
+    "url": "/myapp/static/js/2.57d6f245.chunk.js"
+  },
+  {
+    "revision": "3b4588d94689bfca2b74c84ad7cf8705",
+    "url": "/myapp/index.html"
+  }
+];
